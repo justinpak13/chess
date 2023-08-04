805884635304fa5b9cf1436a9c4a312a4f13981e
Justin Pak <justinpak13@gmail.com>
2023-08-04 17:03:33 -0400

    fen reading and writing capabilities

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     let mut board = Board::starting_pos();
+    let test_fen = String::from("rnb1kbn1/ppp1ppp1/3q3B/3p3p/3P3P/5N1R/PPP1PPP1/RN1QKB2 b Qq - 2 5");
+    board.read_fen(test_fen);
     board.print();
     //let black = vec![0x2654,0x2655, 0x2656, 0x2657, 0x2658, 0x2659];
     //let white = vec![0x265A,0x265B, 0x265C, 0x265D, 0x265E, 0x265F];
@@ -45,6 +47,7 @@ impl Board {
     }
 
     fn read_fen(&mut self, fen: String){
+
         let fen_split: Vec<&str> = fen.split(" ").collect();
         self.color_move = fen_split[1].parse().unwrap();
         self.caslting_rights = String::from(fen_split[2]);
@@ -52,13 +55,14 @@ impl Board {
         self.full_moves = fen_split[5].parse().unwrap();
 
         let coordinates: Vec<&str> = fen_split[0].split("/").collect();
+        self.squares = vec![vec![Piece::Blank;8];8];
 
         for i in 0..8{
             let row = i;
             let mut counter: u32 = 0;
 
             for j in coordinates[row].chars().into_iter(){
-                if counter >= 8 {
+                if counter > 8 {
                     break
                 }
 
