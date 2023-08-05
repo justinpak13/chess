76cb3a4216ee5b381d7d3c80380afb353a9320a3
Justin Pak <justinpak13@gmail.com>
2023-08-05 12:05:13 -0400

    need to switch the letters for the symbols

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -3,14 +3,14 @@ fn main() {
     let test_fen = String::from("rnb1kbn1/ppp1ppp1/3q3B/3p3p/3P3P/5N1R/PPP1PPP1/RN1QKB2 b Qq - 2 5");
     board.read_fen(test_fen);
     board.print();
-    //let black = vec![0x2654,0x2655, 0x2656, 0x2657, 0x2658, 0x2659];
-    //let white = vec![0x265A,0x265B, 0x265C, 0x265D, 0x265E, 0x265F];
-    //for j in white.iter(){
-    //    println!("{:?}",char::from_u32(*j).unwrap());
-    //}
-    //for j in black.iter(){
-    //    println!("{:?}",char::from_u32(*j).unwrap());
-    //}
+    let black = vec![0x2654,0x2655, 0x2656, 0x2657, 0x2658, 0x2659];
+    let white = vec![0x265A,0x265B, 0x265C, 0x265D, 0x265E, 0x265F];
+    for j in white.iter(){
+        println!("{:?}",char::from_u32(*j).unwrap());
+    }
+    for j in black.iter(){
+        println!("{:?}",char::from_u32(*j).unwrap());
+    }
 }
 
 #[derive(Clone,Debug)]
@@ -110,8 +110,9 @@ impl Board {
     fn print(&self) {
         for rows in self.squares.clone().into_iter(){
             for columns in rows {
+                print!("|");
                 match columns {
-                    Piece::Blank => print!("_"),
+                    Piece::Blank => print!(" "),
                     Piece::RookWhite => print!("R"),
                     Piece::RookBlack => print!("r"),
                     Piece::KnightWhite => print!("N"),
@@ -128,7 +129,7 @@ impl Board {
                 }
                 
             }
-            println!("");
+            println!("|");
         }
     } 
 }
